7100ddf91635597079d0dd51d7fabdfcad6333ce
SJopez <mr.sergiolooolll@gmail.com>
2026-08-22 19:06:16 -0400

    brasinham algorithm implemented

--- app/src/main/cpp/engine.cpp
+++ app/src/main/cpp/engine.cpp
@@ -2,6 +2,7 @@
 #include "engine.h"
 #include<string>
 #include <android/log.h>
+#include<vector>
 #define LOG_TAG "StringArt"
 
 #define LOG(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
@@ -22,7 +23,44 @@ string debugArray(float* array, int len){
     return ans;
 }
 
-void HiloramaMain::drawImage(float *image, int imageLen, float *nails, int nailsLen) {
-    string debug = debugArray(image, imageLen);
-    LOG("%s", debug.c_str());
+void addPixel(int x, int y){
+
+}
+
+float Bresinham(int x0, int y0, int x1, int y1, int width, float* image){
+    bool swapAxis = abs(y0 - y1) > abs(x0 - x1);
+
+    if (swapAxis){
+        swap(x0, y0);
+        swap(x1, y1);
+    }
+    if (x0 > x1){
+        swap(x0, x1);
+        swap(y0, y1);
+    }
+
+    float sum = 0;
+    int dx = x1 - x0;
+    int dy = abs(y1 - y0);
+    int dir = (y1 - y0 > 0) ? 1 : -1;
+
+    int index;
+    int y = y0;
+    int p = 2 * dy - dx;
+    for (int i = 0; i <= dx; i++){
+        index = swapAxis ? (x0 + i) * width + y : y * width + (x0 + i);
+        sum += image[index];
+
+        if (p >= 0){
+            y += dir;
+            p -= 2 * dx;
+        }
+        p += 2 * dy;
+    }
+
+    return sum / float(dx + 1);
+}
+
+void HiloramaMain::drawImage(float* image, int imageLen, float* nails, int nailsLen) {
+
 }
